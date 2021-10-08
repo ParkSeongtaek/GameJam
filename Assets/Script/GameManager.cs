a40600317fffa51901f9a213fc9629e845a2d2dc
ParkSeongtaek <56757197+ParkSeongtaek@users.noreply.github.com>
2021-10-09 00:24:26 +0900

    박성택

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,14 +4,52 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static GameManager Instance;
+
+
+    //DB
+
+    int maxScore =0;
+    public void SetMaxScore(int score)
+    {
+        maxScore = score;
+    }
+    public int GetMaxScore()
+    {
+        return maxScore;
+    }
+
+
+
+
     void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+        }
+        else
+        {
+            Destroy(Instance);
+        }
+        maxScore = PlayerPrefs.GetInt("MaxScore",0);
+
+
+    }
+    public void Save()
+    {
+        PlayerPrefs.SetInt("MaxScore", maxScore);
+
+    } 
+    // Update is called once per frame
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void FixedUpdate()
     {
         
     }
